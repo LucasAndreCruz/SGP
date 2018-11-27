123ad201cabb4fa51312e082bb9a8204a5859665
Lucas André <lucasandrelescanocruz@gmail.com>
2018-11-27 19:29:42 -0300

    Add project files.

---
+++ SGP.ApplicationCore/Entity/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGP.ApplicationCore.Entity
+{
+   public class Pedido
+    {
+
+        public int PedidoId { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public DateTime DataDoPedido { get; set; }
+
+        public DateTime? DataDoEnvio { get; set; }
+
+        public DateTime? DataDeEntrega { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public List<ItemDoPedido> Itens { get; set; }
+
+    }
+}
